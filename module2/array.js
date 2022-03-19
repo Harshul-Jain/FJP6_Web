ec6595688b8d7643e09fad75651135e9279f7beb
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-19 16:17:03 +0530

    methods  to array

--- module2/array.js
+++ module2/array.js
@@ -24,3 +24,25 @@ console.log("Element before push "+ eleArr);
 eleArr.push("new item");
 console.log("Element after push "+ eleArr);
 
+//2.pop
+
+console.log("Element before pop: ",eleArr);
+eleArr.pop();
+console.log("Array after pop: ",eleArr);
+
+//3.shift
+
+console.log("Array before shift: ",eleArr);
+eleArr.shift();
+console.log("Array after shift: ",eleArr);
+
+//4. unshift
+
+console.log("Array before unshift: ",eleArr);
+eleArr.unshift("newly added item");
+console.log("Array after unshift: ",eleArr);
+
+// 5.length
+
+let len= eleArr.length;
+console.log(len);
