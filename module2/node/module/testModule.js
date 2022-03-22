e7e485a74dba21678a00081496e12ed74d135f67
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-23 04:19:56 +0530

    calculator module

---
+++ module2/node/module/testModule.js
@@ -0,0 +1,11 @@
+let calc= require('./calculator.js');
+console.log(calc);
+
+console.log(calc.addition(4,6));
+
+console.log(calc.multiply(4,6));
+
+console.log(calc.division(6,4));
+
+console.log(calc.subtraction(6,4));
+
