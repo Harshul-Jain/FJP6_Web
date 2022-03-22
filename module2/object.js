c37cbde5e83503aa0c022169180d937d1a4a9d22
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-22 13:35:37 +0530

    objects in js

---
+++ module2/object.js
@@ -0,0 +1,26 @@
+// empty object
+let obj ={};
+console.log(obj);
+
+let objHj={
+    "Name":"Harshul",
+    "Age": 45,
+    phoneNo:8860503744,
+    lastName:"Jain" // By default lastName is string,no need to put ""
+}
+
+console.log(objHj);
+
+let capAmerica ={
+    Name:'Steve ',
+    Age:100,
+    Friends:['Natasha','Thor','Tony','Bucky','Bruce'],
+    address:{
+        city:"Queens",
+        state:'Haryana'
+    },
+    sayHi:function(){
+        console.log("Cap America says Hi!!!!");
+    }
+}
+console.log(capAmerica);
