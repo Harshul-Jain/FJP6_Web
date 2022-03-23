6507185e49549ab9c40ae7dc16445b42e4b883d5
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-24 02:00:36 +0530

    q1 answer added

--- module2/node/questions/q1.js
+++ module2/node/questions/q1.js
@@ -1,3 +1,13 @@
 //make a folder inside same directory of this file and in that folder make a text file with content
 //new file has been made
 
+let fs =require('fs');
+let path=require('path');
+
+if(!fs.existsSync('new folder')){
+    fs.mkdirSync('new folder');
+}
+
+let newTextFilePath = path.join(__dirname,"new folder","file.txt");
+fs.writeFileSync(newTextFilePath,"new file has been made");
+
