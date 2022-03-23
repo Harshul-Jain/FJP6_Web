64940d63e411afad4ff6cd4fef65823d7685cf1a
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-24 01:52:45 +0530

    add questions

---
+++ module2/node/questions/q3.js
@@ -0,0 +1,3 @@
+//Q. copy index.html file from module1 to a new folder inside module2 having name html.
+
+
---
+++ module2/node/questions/q4.js
@@ -0,0 +1,2 @@
+// read content of unorganised folder and make  an array which has extension name of each file
+
