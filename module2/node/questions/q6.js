9238977dd1a0a1673b00e1213d4b844ca1876dbe
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-24 20:42:33 +0530

    q6  solved

--- module2/node/questions/q6.js
+++ module2/node/questions/q6.js
@@ -2,3 +2,30 @@
 //for example abc.mp3 --> print Audio File
 //            xyz.mp4 --> print Video File
 //            fsd.jpeg -> print Image File
+
+let fileType ={
+    ".mp3": "Audio File",
+    ".mp4": "Video File",
+    ".jpeg": "Image File",
+    ".xlsx":"Excel File",
+    ".pdf" : "PDF File",
+    ".doc" : "Document File",
+    ".zip" : "ZIP File",
+    ".jpg": "Image File"
+}
+
+let fs = require('fs');
+let path = require('path');
+
+let filePath = path.join(__dirname,"..","unorganised");
+let fileArr = fs.readdirSync(filePath);
+
+for(let i=0;i<fileArr.length;i++){
+    let file = fileArr[i];
+    let ext = path.extname(file);
+    if(fileType[ext] != undefined){
+        console.log(fileType[ext]);
+    }
+    else
+        console.log("Other File");
+}
