c43835b55931df36c5cc0690b5084820b827eb92
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-24 20:21:06 +0530

    q5  solved

--- module2/node/questions/q5.js
+++ module2/node/questions/q5.js
@@ -1,2 +1,15 @@
 //move a file
 
+let fs = require('fs');
+
+let path=require('path');
+
+let srcPath = path.join(__dirname,"file1.txt");
+
+let destPath = path.join(__dirname,"new folder","file1.txt");
+
+//copy
+fs.copyFileSync(srcPath,destPath);
+
+//remove
+fs.unlinkSync(srcPath);
