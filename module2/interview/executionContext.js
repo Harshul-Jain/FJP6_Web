0ca478a066c7c7953b26fbf165c0f237f63a2bc2
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-27 01:16:54 +0530

    executionContext

---
+++ module2/interview/executionContext.js
@@ -0,0 +1,18 @@
+console.log(a); //undefined
+
+var a=10;
+
+console.log(a); //10
+
+function fun(){
+    var b= 20;
+    return b;
+}
+
+console.log(c); //undefined
+
+var c=30;
+
+var d=fun(); 
+
+console.log(d); // 20
